20ea75ac62fcff2bffc50e558741758cc38e26b8
Amir <z.amir04lm26@gmail.com>
2025-05-12 20:04:58 +0330

    fix(flights): some issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,10 +13,25 @@ export default async function Home({
 }: Readonly<{
   searchParams: Promise<Record<string, string>>;
 }>) {
-  const params = new URLSearchParams(await searchParams).toString();
-  const url = `${NEXT_PUBLIC_BASE_URL}/${FLIGHT_SEARCH_ENDPOINT}?${params}`;
-  const res = await fetch(url, { next: { revalidate: 3600 } }); // cache for 1 hour
-  const jsonRes: ApiResponse<IFlightSearchResponse> = await res.json();
+  const sp = await searchParams;
+  const hasRequiredParams =
+    sp.originLocationCode &&
+    sp.destinationLocationCode &&
+    sp.departureDate &&
+    sp.adults;
+  let jsonRes: ApiResponse<IFlightSearchResponse> | null = null;
+
+  if (hasRequiredParams) {
+    const params = new URLSearchParams(sp).toString();
+    const url = `${NEXT_PUBLIC_BASE_URL}/${FLIGHT_SEARCH_ENDPOINT}?${params}`;
+
+    try {
+      const res = await fetch(url, { next: { revalidate: 3600 } }); // 1 hour cache
+      jsonRes = await res.json();
+    } catch (err) {
+      console.error("fetching flights failed", err);
+    }
+  }
 
   console.log({ jsonRes });
 
