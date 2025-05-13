c067e1cd860d51d53e5594b555138123e2a45bed
Amir <z.amir04lm26@gmail.com>
2025-05-13 13:17:09 +0330

    build(db): adds memory-limit and lru config to redis

--- src/libs/redis/client.ts
+++ src/libs/redis/client.ts
@@ -8,18 +8,22 @@ export class RedisClient {
 
   public static async new(): Promise<RedisClientType> {
     if (!this.client) {
-      this.client = createClient({
-        url: REDIS_URL,
-      });
+      this.client = createClient({ url: REDIS_URL });
 
       this.client.on("error", (err) => {
         console.error("[Redis Client Error]", err);
       });
 
-      await this.client.connect().catch((err) => {
+      try {
+        await this.client.connect();
+
+        // Redis maxmemory set to 80MB with LRU eviction policy
+        await this.client.configSet("maxmemory", "80mb");
+        await this.client.configSet("maxmemory-policy", "allkeys-lru");
+      } catch (err) {
         console.error("Error connecting to Redis:", err);
         throw err;
-      });
+      }
     }
 
     return this.client;
